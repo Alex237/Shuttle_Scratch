efe4a31b5bd95f13ed02e3aac12dd34767f49ac2
Alex Maxime <mcadevall@phonecontrol.fr>
2013-12-04 14:17:08 +0100

    update ticket

--- controller/Dashboard.php
+++ controller/Dashboard.php
@@ -19,10 +19,19 @@ class Dashboard extends BaseController
     public function index() {
         $this->model->init();
         $idData = $this->session->getUserData();
+        require_once './model/TicketsModel.php';
+        $ticketModel =  new TicketsModel();
+        $ticketModel->init();
+        $ticketList = $ticketModel->loadAllTickets(0,3);
+        $ticketModel->flush();
+        $ticketOpened = $ticketModel->countTicketsOpenBy($idData['idUser']);
+        $ticketModel->close();
         $user = $this->model->loadById($idData['idUser']);
         $user['roles'] = json_decode($user['roles']);
         $this->twig->display('dashboard/overview.html.twig', array(
-            'userData' => $user
+            'userData' => $user,
+            'ticketList' => $ticketList,
+            'ticketOpened'=> $ticketOpened
         ));
         $this->model->close();
     }
